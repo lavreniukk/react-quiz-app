69aab88ddafd1136e4d7711870119003ed08724f
lavreniukk <lavreniuk.andrey@gmail.com>
2023-10-20 14:51:20 +0300

    question service implemented

---
+++ server/services/questionsService.js
@@ -0,0 +1,40 @@
+import { deleteQuestion } from "../controllers/questionController";
+import questionRepository from "../repositories/questionsRepository";
+
+class QuestionService {
+    async getAllQuestions() {
+        try {
+            return await questionRepository.getAll();
+        } catch (error) {
+            throw error
+        }
+    }
+    async getQuestionById(questionId) {
+        try {
+            return await questionRepository.getById(questionId);
+        } catch (error) {
+            throw error
+        }
+    }
+    async addNewQuestion(questionData) {
+        try {
+            return await questionRepository.create(questionData);
+        } catch (error) {
+            throw error
+        }
+    }
+    async updateQuestion(newQuestionData, questionId) {
+        try {
+            return await questionRepository.update(newQuestionData, questionId);
+        } catch (error) {
+            throw error
+        } 
+    }
+    async deleteQuestion(questionId) {
+        try {
+            return await questionRepository.delete(questionId);
+        } catch (error) {
+            throw error
+        }
+    }
+}
