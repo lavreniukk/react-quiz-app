f856468b7edc243f4c0b0f8c62ff975e4e35a190
lavreniukk <lavreniuk.andrey@gmail.com>
2023-10-28 14:01:59 +0300

    quiz model implemented

---
+++ server/models/quiz.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import Question from "./questions";
+
+const quizSchema = mongoose.Schema({
+        title: {
+            type: String,
+        },
+        category: {
+            type: String,
+        },
+        quizQuestions: [{
+            type: Question,
+            required: true,
+            ref: 'Question'
+        }],
+        user: {
+            type: mongoose.Schema.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        userTime: {
+            type: Number,
+            default: undefined
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+export default Quiz;
